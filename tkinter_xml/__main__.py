91d1a3c0f665282523c84a8e851dd6b82709bd6f
Nathan McAvoy <nathanmc158@gmail.com>
2020-10-04 13:41:39 +0100

    Add OOP attr element naming to xml

--- tkinter_xml/__main__.py
+++ tkinter_xml/__main__.py
@@ -8,6 +8,7 @@ class MainPage(Page):
     
     def run(self):
         self.page_children[0].backing_element_generator(self.window).grid()
+        print(self.should_be_visible_on_mainpage)
         self.window.mainloop()
     
     def other_click_fn(self, button):
--- tkinter_xml/elements/BaseElement.py
+++ tkinter_xml/elements/BaseElement.py
@@ -8,15 +8,16 @@ class BaseElement():
         self.children = children
         self.parent_page = parent_page
         for attribute in attributes.keys():
-            if '.' in attribute:
+            if 'x_Name' == attribute:
+                setattr(parent_page, attributes[attribute], self)
+            elif '.' in attribute:
                 custom_attribute_prefix = attribute.split('.')[0]
                 if custom_attribute_prefix in self.custom_attributes:
                     self.custom_attributes[custom_attribute_prefix][".".join(attribute.split('.')[1:])] = attributes[attribute]
                 else:
                     self.custom_attributes[custom_attribute_prefix] = {}
                     self.custom_attributes[custom_attribute_prefix][".".join(attribute.split('.')[1:])] = attributes[attribute]
-                continue
-            if attribute not in self.default_attributes:
+            elif attribute not in self.default_attributes:
                 raise Exception(f"Encountered an invalid attribute on Button: {attribute}")
             else:
                 if attribute in EVENT_HANDLERS:
